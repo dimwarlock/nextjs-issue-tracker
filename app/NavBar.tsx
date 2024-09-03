625cdcb0b4f33b8353ea426d7e6e377c6c7e92c0
dimwarlock <screanvir@gmail.com>
2024-09-03 18:21:02 -0400

    Finalizacion personalizacion Navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,9 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiFillAppstore } from "react-icons/ai";
+import classnames from 'classnames';
 
 const NavBar = () => {
   const links = [
@@ -14,13 +17,19 @@ const NavBar = () => {
     },
   ]
 
+  const currentPath = usePathname();
+
   return (
     <nav className='flex space-x-5 border-b mb-5 px-5 h-14 items-center'>
         <Link href='/'><AiFillAppstore /></Link>
         <ul className='flex space-x-5'>
             {links.map(link => 
                 <Link key={link.href}
-                    className='text-zinc-400 hover:text-zinc-200 transition-colors'
+                    className={classnames({
+                      'text-zinc-400': true,
+                      'hover:text-zinc-200 transition-colors': link.href !== currentPath,
+                      'text-purple-600': link.href === currentPath
+                    })}
                     href={link.href}
                 >
                 {link.label}
