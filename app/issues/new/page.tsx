d0141862917eba86d64c3b2e33801b41ca8e8a8d
dimwarlock <screanvir@gmail.com>
2024-09-04 12:37:47 -0400

    Issue de Form a BDD

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,6 +5,7 @@ import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import {useForm, Controller} from 'react-hook-form';
 import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 interface IssueForm {
   title: string;
@@ -12,6 +13,7 @@ interface IssueForm {
 }
 
 const newIssuePage = () => {
+  const router = useRouter();
   const {register, control, handleSubmit} = useForm<IssueForm>();
 
   return (
@@ -19,6 +21,7 @@ const newIssuePage = () => {
       className='max-w-xl ml-4 space-y-2' 
       onSubmit={handleSubmit(async (data)=> {
         await axios.post('/api/issues', data)
+        router.push('/issues')
       })}
     >
         <TextField.Root size="3" color="iris" variant="soft" placeholder="Titulo" {...register('title')} />
