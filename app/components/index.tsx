35a8a2a911589bc36ff72c77013b17d2178de2e3
dimwarlock <screanvir@gmail.com>
2024-09-05 01:13:15 -0400

    Refactorización 2

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,9 +1,6 @@
-import {Link} from "./Link";
-import ErrorMsg from "./ErrorMsg"
-import { IssueStatusBadge } from "./IssueStatusBadge";
-import Spinner from "./Spinner";
 
-export {Link}
-export {ErrorMsg}
-export {IssueStatusBadge}
-export {Spinner}
+
+export {Link} from "./Link";
+export {default as ErrorMsg} from "./ErrorMsg";
+export {IssueStatusBadge} from "./IssueStatusBadge";
+export {default as Spinner} from "./Spinner";
