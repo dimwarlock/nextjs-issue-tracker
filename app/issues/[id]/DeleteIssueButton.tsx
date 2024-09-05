3357a3d0d2f0d4346c733abf7c7c09e307c2a451
dimwarlock <screanvir@gmail.com>
2024-09-05 11:01:42 -0400

    Delete Alert Dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
+'use-client'
 import { TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import Link from 'next/link';
 import React from 'react'
 
@@ -9,7 +10,27 @@ interface Props {
 
 const DeleteIssueButton = ({id}: Props) => {
   return (
-    <Button color='red'><TrashIcon /><Link href={`\${id}`}>Delete Issue</Link></Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'><TrashIcon /><Link href={`\${id}`}>Delete Issue</Link></Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>
+          Confirm Deletion
+        </AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to confirm this deletion? This action cannot be undone.
+        </AlertDialog.Description>
+        <Flex mt='4' gap='3'>
+          <AlertDialog.Cancel>
+            <Button variant='soft'>Cancel</Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color='red'>Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
