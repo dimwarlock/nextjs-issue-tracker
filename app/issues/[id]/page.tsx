fe13a7be1db5578b5bccaf5fdf25239a5a3b90b2
dimwarlock <screanvir@gmail.com>
2024-09-04 23:48:03 -0400

    Estilo de Detalle de Issue

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import { IssueStatusBadge } from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
@@ -17,11 +19,15 @@ const IssueDetailPage = async ({params}: Props) => {
     notFound();
 
   return (
-    <div>
-        <p>{issue.title}</p>
-        <p>{issue.description}</p>
-        <p>{issue.status}</p>
-        <p>{issue.createdAt.toDateString()}</p>
+    <div className='ml-4'>
+        <Heading>{issue.title}</Heading>
+        <Flex className='space-x-3 my-2'>
+            <IssueStatusBadge status={issue.status}/>
+            <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card>
+            {issue.description}
+        </Card>
     </div>
   )
 }
