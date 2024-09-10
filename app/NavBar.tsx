bdf4070e6bd6ae653accbaef8e9ac473f4cb1fdf
dimwarlock <screanvir@gmail.com>
2024-09-10 09:56:36 -0300

    Menu de Logout con Avatar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { AiFillAppstore } from "react-icons/ai";
 import classnames from 'classnames';
 import {useSession} from 'next-auth/react'
-import { Box, Container, Flex } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
 const NavBar = () => {
   const {status, data: session} = useSession();
@@ -46,8 +46,35 @@ const NavBar = () => {
           </Flex>
 
           <Box>
-            {status === 'unauthenticated' && <Link href='/api/auth/signin'>Login</Link>}
-            {status === 'authenticated' && <Link href='/api/auth/signout'>Log Out</Link>}
+            {status === 'unauthenticated' && (
+              <Link href='/api/auth/signin'>Login</Link>
+            )}
+
+            {status === 'authenticated' && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar 
+                    src={session.user!.image!} 
+                    fallback='?'
+                    size='2' 
+                    radius='full' 
+                    className='cursor-pointer'
+                  />
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size='2'>
+                     {session.user!.email}
+                    </Text>
+                  </DropdownMenu.Label>
+
+                  <DropdownMenu.Item>
+                    <Link href='/api/auth/signout'>Log Out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
           </Box>
         </Flex>
       </Container>
