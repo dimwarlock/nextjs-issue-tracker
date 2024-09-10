8b52854306954af621e70f074eee8cf03920a9e2
dimwarlock <screanvir@gmail.com>
2024-09-10 10:11:32 -0300

    Refactorizar NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -8,7 +8,60 @@ import {useSession} from 'next-auth/react'
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
 const NavBar = () => {
+  return (
+    <nav className='border-b mb-5 px-5 py-3 h-14'>
+      <Container>
+        <Flex justify='between'>
+          <Flex align='center' gap='3'>
+            <Link href='/'><AiFillAppstore /></Link>
+            <NavLinks />
+          </Flex>
+            <AuthStatus />
+        </Flex>
+      </Container>
+    </nav>
+  )
+}
+
+const AuthStatus = ()=> {
   const {status, data: session} = useSession();
+
+  if (status === 'loading')
+    return null;
+
+  if (status === 'unauthenticated')
+    return <Link href='/api/auth/signin' className='nav-link'>Login</Link>
+  return(
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar 
+            src={session!.user!.image!} 
+            fallback='?'
+            size='2' 
+            radius='full' 
+            className='cursor-pointer'
+          />
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size='2'>
+              {session!.user!.email}
+            </Text>
+          </DropdownMenu.Label>
+
+          <DropdownMenu.Item>
+            <Link href='/api/auth/signout'>Log Out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  )
+}
+
+const NavLinks = ()=> {
+  const currentPath = usePathname();
 
   const links = [
     {
@@ -21,64 +74,21 @@ const NavBar = () => {
     },
   ]
 
-  const currentPath = usePathname();
-
   return (
-    <nav className='border-b mb-5 px-5 py-3 h-14'>
-      <Container>
-        <Flex justify='between'>
-          <Flex align='center' gap='3'>
-            <Link href='/'><AiFillAppstore /></Link>
-            <ul className='flex space-x-5'>
-              {links.map(link => 
-                  <li key={link.href}>
-                    <Link className={classnames({
-                        'text-black': link.href === currentPath,
-                        'text-zinc-500 hover:text-black transition-colors': link.href !== currentPath
-                      })}
-                      href={link.href}
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                )}
-            </ul>
-          </Flex>
-
-          <Box>
-            {status === 'unauthenticated' && (
-              <Link href='/api/auth/signin'>Login</Link>
-            )}
-
-            {status === 'authenticated' && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar 
-                    src={session.user!.image!} 
-                    fallback='?'
-                    size='2' 
-                    radius='full' 
-                    className='cursor-pointer'
-                  />
-                </DropdownMenu.Trigger>
-
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size='2'>
-                     {session.user!.email}
-                    </Text>
-                  </DropdownMenu.Label>
-
-                  <DropdownMenu.Item>
-                    <Link href='/api/auth/signout'>Log Out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+    <ul className='flex space-x-5'>
+      {links.map(link => 
+          <li key={link.href}>
+            <Link className={classnames({
+                'nav-link': link.href !== currentPath,
+                'text-black': link.href === currentPath,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        )}
+    </ul>
   )
 }
 
