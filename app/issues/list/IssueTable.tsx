ece00a39cf4fb4cca8c2b51db581c330368c2b4f
dimwarlock <screanvir@gmail.com>
2024-09-11 15:08:06 -0300

    Refactorización

---
+++ app/issues/list/IssueTable.tsx
@@ -0,0 +1,63 @@
+import NextLink from 'next/link'
+import { ArrowUpIcon } from '@radix-ui/react-icons'
+import { Link } from '@/app/components/index'
+import { IssueStatusBadge } from '../../components/IssueStatusBadge'
+import {Table} from '@radix-ui/themes'
+import { Issue, Status } from '@prisma/client'
+
+export interface IssueQuery {
+    status: Status, 
+    orderBy: keyof Issue, 
+    page: string
+}
+
+interface Props {
+    searchParams: IssueQuery,
+    issues: Issue[]
+}
+
+const columns : {label: string, value: keyof Issue, className?: string}[] = [
+    {label: 'Issue', value: 'title'},
+    {label: 'Status', value: 'status', className: 'hidden md:table-cell'},
+    {label:'Created', value:'createdAt', className: 'hidden md:table-cell'}
+    ];
+
+export const columnNames = columns.map(column=> column.value);
+
+const IssueTable = ({searchParams, issues}: Props) => {
+  return (
+    <Table.Root variant='surface' className='mt-5'>
+      <Table.Header>
+        <Table.Row>
+          {columns.map(column=> (
+          <Table.ColumnHeaderCell key={column.value} className={column.className}> 
+                <NextLink href={{query: {...searchParams, orderBy: column.value}}}>
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && <ArrowUpIcon className='inline' />}
+          </Table.ColumnHeaderCell>
+          ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {issues.map(issue => (
+          <Table.Row key={issue.id}>
+            <Table.Cell>
+              <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+              <div className='block md:hidden'>
+                <IssueStatusBadge status={issue.status}/>
+                <div>
+                  {issue.createdAt.toDateString()}
+                </div>
+              </div>
+            </Table.Cell>
+            <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status}/></Table.Cell>
+            <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  )
+}
+
+export default IssueTable
